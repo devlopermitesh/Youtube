9ab678a58ce8d31523faf3d5c7f4368e8697e89c
unknown <gehlotmitesh63@gmail.com>
2025-06-24 12:16:32 +0530

    authentication flow

--- src/module/auth/ui/components/auth-button.tsx
+++ src/module/auth/ui/components/auth-button.tsx
@@ -1,12 +1,23 @@
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from "@clerk/nextjs";
 import { UserCircleIcon } from "lucide-react";
 
 const AuthButton=()=>{
 return(
+    <>
+    <SignedIn>
+        <UserButton/>
+    </SignedIn>
+    <SignedOut>
+        <SignInButton mode="modal">
     <Button variant={'outline'} className="px-6 py-2 text-sm font-medium text-blue-600 hover::text-blue-500 border-blue-500/2 rounded-full shadwo-none [&_svg]:size-5">
 <UserCircleIcon/>
 Sign in
     </Button>
+    </SignInButton>
+
+    </SignedOut>
+    </>
 )
 }
 export default AuthButton; 
