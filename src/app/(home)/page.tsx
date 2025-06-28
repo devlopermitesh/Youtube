d6d81663365881d9aa35cafb18da3ab669c7bf70
unknown <gehlotmitesh63@gmail.com>
2025-06-28 13:07:53 +0530

    feat: test husky hook

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,48 @@
-import { HomeLayout } from "@/module/home/ui/layout/home-layout";
 
 export default function Home() {
   return (
   
-   <h2 className="text-pink-500 text-2xl font-bold">Hello world</h2>
+   <h2 className="text-pink-500 text-2xl font-bold">
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    Hello world
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </h2>
    
   );
 }
